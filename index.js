dfc5f0d39df527da2f2d5aa2ca09b076dd7caba4
Shaunak G <hello@srg.codes>
2021-05-17 00:26:25 +1000

    fixed CORS origin

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(limiter);
 app.use(express.json());
 
 app.use(require('cors')({
-    origin: "*"
+    origin: "https://c2n.srg.id.au"
 }));
 
 app.all("/", (req, res) => res.redirect("https://c2n.srg.id.au"))
